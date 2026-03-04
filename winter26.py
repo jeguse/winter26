e1d77080a561a97f6d5fffc58330a692f8e9a622
Jonah Guse <jonahguse49@gmail.com>
2026-03-04 13:08:11 -0800

    rnext_row_data = iter(row_data_list[23])

--- winter26.py
+++ winter26.py
@@ -6,11 +6,11 @@ n = 4
 def dotprod(x,y):
     total = 0
     for i in range(n):
-        total += int(x[2*i]) * int(y[2*i+1]) + int(x[2*i+1]) * int(y[2*i])
-    return total % 2
+        total = total ^ (x[2 * i] and y[2 * i + 1]) ^ (x[2 * i + 1] and y[2 * i])
+    return total
 
 def isotropic(x,y):
-    return not bool(dotprod(x,y))
+    return not dotprod(x,y)
 
 def rrefcheck(x,y):
     if x < y or x[y.find('1')] == '1': return False
@@ -79,11 +79,85 @@ vecs = paulisi
 # print(len(allspaces))
 
 n = 5
+# count = 0
+# for matrix in product(product('01', repeat=n+1), repeat=n):
+#     count += 1
+# print(count)
+
+
+# Gives all nonzero boolean vectors of length n+1, which is enough data to define the next row at each step of our algorithm
+row_data_list = list(product([True, False], repeat=n+1))
+# Remove the all-False vector (improves efficiency very slightly, more importantly this makes later code more convenient to write)
+row_data_list.pop()
+
+# A small note: there is an issue that I am building all 2-row matrices before building a single 3-row matrix,
+# rather than a depth-first approach
+# TODO I would like to refactor the code so it uses recursion to generate the RREF matrices in a depth-first manner
+# Keeping track of the pivoting indices for each matrix separately is the primary concern, though,
+# and I'm optimistic that this is not a large issue.
+pivot_indices = []
+
+
+# This list will eventually hold boolean RREFs for every possible maximal isotropic subspace
+# We set this list to be the unique 0-row matrix
+all_spaces = list()
+all_spaces.append([])
+
+# We will want n many rows, so we iterate over the number of rows we've computed so far
+for row_number in range(n):
+
+    # Set up a place to store all partial matrices of height row_number + 1
+    temp = list()
+
+    # For each matrix-in-progress of height row_number...
+    for mat in all_spaces:
+        # TODO would be preferred to not recompute pivot_indices over and over
+        pivot_indices = []
+        for prev_row in mat:
+            pivot_indices += [prev_row.index(True)]
+        
+        # ... and for each candidate for the next row
+        for row_data in row_data_list:
+            
+            # Get the n+1 bits of data required to build the next row into a form we can use easily
+            next_row_data = iter(row_data)
+
+            # Build the next row by allowing n+1 bits of data to control our n+1 degrees of freedom...
+            # Start by setting everything to 0
+            next_row = [False] * (2 * n)
+            # Fill in the bits that are not "too far to the left" or "under previous pivots" using the n+1 given bits of data
+            for index_within_row in range(n - row_number - 1, 2 * n):
+                if index_within_row not in pivot_indices:
+                    next_row[index_within_row] = next(next_row_data)
+
+            # Check RREF condition and update pivot_indices for future RREF checks
+            # TODO updating pivot_indices currently doesn't help, since we are just recomputing it every time
+            if (True in next_row) and (next_row.index(True) < min(pivot_indices + [3 * n])):
+                pass
+                # pivot_indices += [next_row.index(True)]
+            else:
+                continue
+
+            # Check isotropic condition
+            conflict = False
+            for prev_row in mat:
+                if not isotropic(next_row, prev_row):
+                    conflict = True
+                    # break
+            if conflict:
+                continue
+            
+            # RREF and isotropic conditions satisfied, so append this row to the in-progress-matrix
+            # and store this matrix-in-progress of height row_number + 1
+            temp.append(mat + [next_row])
+
+    # Update all_spaces so it now stores the height row_number + 1 matrices in preparation for the next iteration
+    all_spaces = temp
+
 count = 0
-for matrix in product(product('01', repeat=n+1), repeat=n):
+for space in all_spaces:
     count += 1
 print(count)
-
 
 # TODO: Initialize the bipartite graph
 # TODO: Adding the COB matrices to the edges
